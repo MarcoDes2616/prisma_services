a1b8ee3d48004b872695956232951f7a13ef81f8
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-05-12 17:06:04 -0500

    create routes-instance axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
 import './App.css'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import ProtectedRoutes from './components/ProtectedRoutes'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
   return (
-    <>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route element={<ProtectedRoutes/>}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   )
 }
 
---
+++ src/components/ProtectedRoutes.jsx
@@ -0,0 +1,15 @@
+import { Navigate, Outlet} from 'react-router-dom';
+
+const ProtectedRoutes = () => {
+    const userToken = localStorage.getItem('token');
+
+
+    if (userToken) {
+        return <Outlet />
+    } else {
+        return <Navigate to='/' />
+    }
+
+};
+
+export default ProtectedRoutes;
